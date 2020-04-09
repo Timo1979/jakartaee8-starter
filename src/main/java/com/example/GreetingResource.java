1c637ae18393766100a7ee717f233143f136d4b8
Tim Lukashevich <efreet@tut.by>
2020-04-09 18:14:18 +0300

    tried web.xml. Without any success

--- src/main/java/com/example/GreetingResource.java
+++ src/main/java/com/example/GreetingResource.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -13,6 +14,7 @@ import static javax.ws.rs.core.Response.ok;
 
 @Path("greeting")
 @RequestScoped
+@RolesAllowed("perm_engineer")
 public class GreetingResource {
 
     @Inject
@@ -23,5 +25,9 @@ public class GreetingResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response greeting(@PathParam("name") String name) {
         return ok(this.greetingService.buildGreetingMessage(name)).build();
+    }
+
+    public GreetingResource() {
+        System.out.println("constructor");
     }
 }
